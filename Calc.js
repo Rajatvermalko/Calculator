5c8c3c89d3b747ecd5846be27c795cec4ca37bf7
Rajat Verma <104197270+Rajatvermalko@users.noreply.github.com>
2022-07-20 21:26:08 +0530

    new fix

--- Calc.js
+++ Calc.js
@@ -1,32 +1,48 @@
-let num={0,0};
-
 $(function()
 {
+     let num=[0,0,0,0,0,0,0,0,0,0];
+     let op=["","","","","","","","",""];
     var display = $('#screen');
     display.val(0);
     let flag=0;
 
     $('button').click(function inputData()
     {
-        if(display.val() != 0)
-        {
-            display.val(display.val() + $(this).val());
-        }
-        else
-        {
-            display.val($(this).val());
-        }
-        num[flag] = $(this).val();
-
+        wh(display.val() != 0)    //take input add value 
+        display.val(display.val() + $(this).val());
+       
+       display.val($(this).val()); //display finalized value 
+       
         if(!(Number.isInteger(display.val())))
         {
-        
-        
+        op[flag] = ($(this).val());   //operator input
+        num[flag] = display.val($(this).val()); //take num out for calculattions
         }
-}
-);
-    
-    }
-    );
+    });
+      flag++;                                 //increase flag value
+       if(op != "=")                         //not pressed equal to go back
+        inputData();
 
+        switch(op[flag-1])
+        {
+            case "*":
+              result= num[0]*num[1];
+                break;
+            case "-":
+               result= num[0]-num[1];
+                break;
+            case "+":
+               result= num[0]+num[1];
+                break;
+            case "/":
+                result= num[0]/num[1];
+                break;
+            case "**":
+                result= num[0]**num[1];
+                break;
+            default :                 //user pressed =equalto atlast
+            result=num[0]
+
+        }
+    });
 $("#clear").click(reset());
